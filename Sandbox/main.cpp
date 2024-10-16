ff222943dfd51ed59afdbb400fdf40f3a9d8f2a6
solfrak <60186753+solfrak@users.noreply.github.com>
2024-10-16 10:30:26 -0400

    Integrate Googletest Framework (#68)

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,146 +1,125 @@
 #include <memory>
-#include <core/rendering/Renderer.hpp>
-#include <core/rendering/VertexArray.hpp>
 
 #include "core/inputs/input.h"
 #include "core/logging/Logger.h"
-#include "core/logging/LoggingFactory.h"
+#include "core/logging/LoggerFactory.h"
 #include "core/window/window.h"
+#include "core/window/WindowManager.h"
 #include "core/window/window_configuration.h"
 #include "core/window/window_factory.h"
-
 #include "scene/components.h"
-#include "scene/entity.h"
+#include "scene/ECS.h"
+#include "scene/query_builder.h"
+#include "scene/system_manager.h"
 
-#include "scene/scene.h"
 
-#include "core/Profiling/profiler.h"
-#include "core/rendering/orthographic_camera.h"
-#include "core/rendering/Texture.h"
-#include "GLFW/glfw3.h"
-#include "platform/opengl/opengl_shader.h"
 
-unsigned int createBasicShader();
-unsigned int createTextureShader();
-
-int main(void)
+struct myData : hive::IComponent
 {
-	ENABLE_PROFILING;
-	hive::Logger::setLogger(hive::LoggingFactory::createLogger(hive::LogOutputType::Console, hive::LogLevel::Info));
+    float x, y;
 
-    //Init Logging
-    hive::Logger::setLogger(hive::LoggingFactory::createLogger(hive::LogOutputType::Console, hive::LogLevel::Debug));
-
-    //Init Window
-    hive::WindowConfiguration configuration;
-    configuration.set(hive::WindowConfigurationOptions::CURSOR_DISABLED, true);
-    const auto window = std::unique_ptr<hive::Window>(hive::WindowFactory::Create("Hive Engine", 800, 600, configuration));
-
-    //Init Input
-    hive::Input::init(window->getNativeWindow());
-
-
-    /*unsigned int shaderProgram = createBasicShader();
-    unsigned int textureShader = createTextureShader();*/
-    hive::OrthographicCamera m_Camera(-1.0f, 1.0f, -1.0f, 1.0f);
-
-    std::string fragmentPath = "../HiveEngine/assets/shaders/basicColorShader.frag.glsl";
-    std::string vertexPath = "../HiveEngine/assets/shaders/basicColorShader.vert.glsl";
-
-    std::shared_ptr<hive::OpenglShader> colorShader = std::make_shared<hive::OpenglShader>(vertexPath, fragmentPath);
-
-    fragmentPath = "../HiveEngine/assets/shaders/textureShader.frag.glsl";
-    vertexPath = "../HiveEngine/assets/shaders/textureShader.vert.glsl";
-
-    std::shared_ptr<hive::OpenglShader> textureShader = std::make_shared<hive::OpenglShader>(vertexPath, fragmentPath);
-
-    std::shared_ptr<hive::VertexArray> vertexArray;
-    std::shared_ptr<hive::VertexArray> squareVA;
-
-    vertexArray.reset(hive::VertexArray::create());
-
-    float vertices[3 * 7] = {
-            -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
-            0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
-            0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
-    };
-
-    std::shared_ptr<hive::VertexBuffer> vertexBuffer = std::shared_ptr<hive::VertexBuffer>(hive::VertexBuffer::create(vertices, sizeof(vertices)));
-    hive::BufferLayout layout = {
-            { hive::ShaderDataType::Float3, "a_Position" },
-            { hive::ShaderDataType::Float4, "a_Color" }
-    };
-    vertexBuffer->setLayout(layout);
-
-    vertexArray->addVertexBuffer(vertexBuffer);
-
-    uint32_t indices[3] = { 0, 1, 2 };
-    std::shared_ptr<hive::IndexBuffer> indexBuffer;
-    indexBuffer.reset(hive::IndexBuffer::create(indices, sizeof(indices)));
-    vertexArray->setIndexBuffer(indexBuffer);
-
-    squareVA.reset(hive::VertexArray::create());
-
-    float squareVertices[5 * 4] = {
-            -0.75f, -0.75f, 0.0f,  0.0f, 0.0f,
-            0.75f, -0.75f, 0.0f,  1.0f, 0.0f,
-            0.75f,  0.75f, 0.0f,  1.0f, 1.0f,
-            -0.75f,  0.75f, 0.0f, 0.0f, 1.0f
-    };
-
-    std::shared_ptr<hive::VertexBuffer> squareVB = std::shared_ptr<hive::VertexBuffer>(hive::VertexBuffer::create(squareVertices, sizeof(squareVertices)));
-    squareVB->setLayout({
-                                {hive::ShaderDataType::Float3, "a_Position"},
-                                { hive::ShaderDataType::Float2, "a_TexCoord" }
-                        });
-    squareVA->addVertexBuffer(squareVB);
-
-    uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-    std::shared_ptr<hive::IndexBuffer> squareIB;
-    squareIB.reset(hive::IndexBuffer::create(squareIndices, sizeof(squareIndices)));
-    squareVA->setIndexBuffer(squareIB);
-
-    std::shared_ptr<hive::Texture2D> m_Texture = hive::Texture2D::Create("../HiveEngine/assets/textures/Checkerboard.png");
-
-    textureShader->bind();
-    textureShader->uploadUniformInt("u_Texture", 0);
-  
-    // TEST ECS
-	hive::Scene scene = {};
-	hive::Entity entity = scene.createEntity("Test");
-	hive::Entity entity_no_name = scene.createEntity();
-	std::cout << entity.toString() << std::endl;
-	std::cout << entity_no_name.toString() << std::endl;
-	auto& tag = entity_no_name.replaceComponent<hive::TagComponent>();
-	tag.Tag = "Replace";
-	std::cout << scene.toString() << std::endl;
-  
-    float angle = 0.0f;
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(reinterpret_cast<GLFWwindow*>(window->getNativeWindow())))
+    std::string toString() override
     {
-    	BLOCK_PROFILING("BLOCK TEST", hive::BlockStatus::ON, hive::colors::Green);
-        angle += 0.5f;
-
-        m_Camera.setPosition({ 0.5f, 0.0f, 0.0f });
-        m_Camera.setRotation(angle);
-
-        hive::Renderer::beginScene(m_Camera);
-
-        m_Texture->bind();
-        hive::Renderer::submitGeometryToDraw(squareVA, textureShader);
-
-        hive::Renderer::submitGeometryToDraw(vertexArray, colorShader);
-
-        hive::Renderer::endScene();
-
-
-        /* Poll for and process events */
-        window->onUpdate();
-    	END_BLOCK_PROFILING;
+        return std::string(std::to_string(x) + " " + std::to_string(y));
     }
-	DUMP_PROFILING("test.prof");
+};
+
+class TestSystem : public hive::System
+{
+
+public:
+    void update(float deltaTime) override
+    {
+        if(hive::Input::getKeyDown(hive::KEY_SPACE))
+        {
+            auto window = hive::WindowManager::getCurrentWindow();
+            auto config = window->getConfiguration();
+
+            config.toggle(hive::WindowConfigurationOptions::CURSOR_DISABLED);
+            window->updateConfiguration(config);
+        }
+    }
+
+    void init() override
+    {
+        auto query = hive::QueryBuilder<myData>();
+        for(auto [entity, data] : query.each())
+        {
+            hive::Logger::log("We have an entity", hive::LogLevel::Info);
+        }
+    }
+};
+
+
+void setupLogger(const hive::LogOutputType type, const hive::LogLevel level)
+{
+    hive::Logger::init(hive::LoggerFactory::createLogger(type, level));
+}
+
+void setupWindow(const hive::WindowConfiguration configuration)
+{
+    auto window = std::shared_ptr<hive::Window>(hive::WindowFactory::Create("Hive Engine", 800, 600, configuration));
+    hive::WindowManager::setCurrentWindow(window);
+}
+
+void setupInput()
+{
+    auto window = hive::WindowManager::getCurrentWindow();
+    hive::Input::init(window->getNativeWindowData());
+}
+
+void setupEcs()
+{
+    //ECS
+    hive::ECS::init();
+
+    // auto registry = hive::ECS::getCurrentRegistry();
+    // auto entity = registry->createEntity();
+    auto entity = hive::ECS::createEntity();
+    hive::ECS::addComponent<myData>(entity);
+
+    hive::ECS::registerSystem(new TestSystem(), "TestSystem");
+}
+
+
+void shutdown()
+{
+    hive::ECS::shutdown();
     hive::Input::shutdown();
+    hive::WindowManager::setCurrentWindow(nullptr);
+    hive::Logger::shutdown();
+}
+
+void init()
+{
+    setupLogger(hive::LogOutputType::Console, hive::LogLevel::Debug);
+
+    hive::WindowConfiguration configuration;
+    // configuration.set(hive::WindowConfigurationOptions::CURSOR_DISABLED, true);
+    setupWindow(configuration);
+
+    setupInput();
+
+    setupEcs();
+}
+
+int main()
+{
+    init();
+
+    //Game loop
+    auto window = hive::WindowManager::getCurrentWindow();
+    while(!window->shouldClose()) {
+        //Swap the buffer
+        window->onUpdate();
+
+        //Run all the systems
+        hive::ECS::updateSystems(0);
+    }
+
+    window.reset();
+
+    shutdown();
+
     return 0;
 }
